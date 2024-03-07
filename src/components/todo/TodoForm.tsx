abeacac7a810e8e0096e6c0a00f3140d73ddecc3
illu_y <ii_2002@naver.com>
2024-03-07 11:12:12 +0900

    todo form, list 셋팅

--- src/components/todo/TodoForm.tsx
+++ src/components/todo/TodoForm.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import { Todo } from '../../types/todoType';
 
 export const TodoForm = () => {
-    const onSubmit = (e: React.FormEvent<HTMLElement>) =>{
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
         e.preventDefault();
-        
+
+        const formData = new FormData(e.currentTarget);
+        const title = formData.get("title") as string;
+        const content = formData.get("content") as string;
+
+        const nextTodo: Todo = {
+            id:crypto.randomUUID(),
+            title,
+            content,
+            isDone :false,
+            deadline : new Date().toISOString(),
+        }
+
     }
 
   return (
-    <section onSubmit={onSubmit}>
-        <form >
+    <section >
+        <form onSubmit={onSubmit}>
             <input type="text" name='title' />
             <textarea name="content" id=""></textarea>
             <button type='submit'>제출하기</button>
