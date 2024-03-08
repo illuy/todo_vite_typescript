120193f574ddab0de36c0d8f0be2291c46c8e42b
illu_y <ii_2002@naver.com>
2024-03-08 13:20:23 +0900

    level3 start

---
+++ src/app/todoApi.ts
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const todoApi = createApi({
+  reducerPath: 'todoApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5001/' }),
+  endpoints: (builder) => ({
+    getTodos: builder.query<any, void>({
+      query: () => 'todos',
+    }),
+    addTodo: builder.mutation<any, Partial<{ title: string; content: string }>>({
+      query: (body) => ({
+        url: 'todos',
+        method: 'POST',
+        body,
+      }),
+    }),
+    deleteTodo: builder.mutation<any, string>({
+      query: (id) => ({
+        url: `todos/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+    toggleTodoDone: builder.mutation<any, { id: string; isDone: boolean }>({
+      query: ({ id, isDone }) => ({
+        url: `todos/${id}`,
+        method: 'PATCH',
+        body: { isDone },
+      }),
+    }),
+  }),
+});
+
+export const { useGetTodosQuery, useAddTodoMutation, useDeleteTodoMutation, useToggleTodoDoneMutation } = todoApi;
